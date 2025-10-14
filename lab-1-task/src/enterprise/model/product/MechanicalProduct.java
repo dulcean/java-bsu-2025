18420aec0c5d54265e792c29a74a915e3471d875
VadimO111 <liepievar@gmail.com>
2025-10-15 00:28:53 +0300

    add Product class

---
+++ lab-1-task/src/enterprise/model/product/MechanicalProduct.java
@@ -0,0 +1,12 @@
+package enterprise.model.product;
+
+public class MechanicalProduct extends Product {
+    public MechanicalProduct(String id, String name, int productionTime) {
+        super(id, name, productionTime);
+    }
+
+    @Override
+    public String getCategory() {
+        return "Mechanical";
+    }
+}
