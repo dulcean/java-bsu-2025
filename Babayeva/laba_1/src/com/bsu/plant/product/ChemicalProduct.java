98c3066005954df550ea26f10e11800f882ad584
PalinaBabayeva <palinababayeva@gmail.com>
2025-10-17 23:51:29 +0300

    Add files via upload

---
+++ Babayeva/laba_1/src/com/bsu/plant/product/ChemicalProduct.java
@@ -0,0 +1,13 @@
+package com.bsu.plant.product;
+
+public class ChemicalProduct extends Product {
+    
+    public ChemicalProduct(String id, String name, int productionTime) {
+        super(id, name, productionTime);
+    }
+
+    @Override
+    public String getCategory() {
+        return "Chemical";
+    }
+}
---
+++ Babayeva/laba_1/src/com/bsu/plant/productlines/MechanicalLine.java
@@ -0,0 +1,21 @@
+package com.bsu.plant.productlines;
+
+import com.bsu.plant.product.MechanicalProduct;
+import com.bsu.plant.product.Product;
+import java.util.List;
+
+public class MechanicalLine extends ProductionLine<MechanicalProduct> {
+    
+    public MechanicalLine(String lineId, double efficiency) {
+        super(lineId, efficiency);
+    }
+
+    public MechanicalLine(String lineId, List<MechanicalProduct> products, double efficiency) {
+        super(lineId, products, efficiency);
+    }
+
+    @Override
+    public boolean canProduce(Product product) {
+        return product instanceof MechanicalProduct;
+    }
+}
